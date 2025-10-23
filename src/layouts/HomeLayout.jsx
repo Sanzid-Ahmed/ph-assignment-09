313c407c1f905cbdf753afe6292db72fe48199ab
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-10-23 17:16:32 +0600

    added nav bar

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Outlet } from 'react-router';
+import Header from '../components/Header';
 
 const HomeLayout = () => {
     return (
         <div>
-            <header></header>
-            <main></main>
+            <header>
+                <Header></Header>
+            </header>
+            <main>
+                <Outlet></Outlet>
+            </main>
             <footer></footer>
         </div>
     );
