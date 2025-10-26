9431895a53b2acac114ab6b516d44ae18a8e8c90
Sanzid Ahmed <sanzidahmed313@gmail.com>
2025-10-26 11:47:23 +0600

    lasr commit

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,16 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDV30KfyKUG-NjV27xDqB847Kl8NwKGkLM",
-  authDomain: "pet-care-9edab.firebaseapp.com",
-  projectId: "pet-care-9edab",
-  storageBucket: "pet-care-9edab.firebasestorage.app",
-  messagingSenderId: "820683291857",
-  appId: "1:820683291857:web:e588e4247f8328276519f0"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
